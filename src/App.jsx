41b62a572a8d472fd6fbed67e53586c9b8793ca6
Brian <galyenb@vcu.edu>
2024-01-24 17:40:43 -0500

    attempting to fix Header build bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/ui/Header.jsx';
 import Footer from './components/ui/Footer.jsx';
+import Header from './components/ui/Header.jsx';
 // import Background from './components/ui/Background';
 import './assets/css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +9,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="d-flex flex-column">
-      {/* <Background /> */}
       <Header />
       <main id="coverImage">
         <Outlet />
