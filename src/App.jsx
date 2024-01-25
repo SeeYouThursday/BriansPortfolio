976ad0e1749185488914e63c4967143317c3b837
Brian <galyenb@vcu.edu>
2024-01-24 22:58:51 -0500

    changed to lowercase app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import Header from './components/Header';
 // import Background from './components/ui/Background';
 import { Outlet } from 'react-router-dom';
-import './assets/css/App.css';
+import './assets/css/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/UI/Footer.jsx';
 import Header from './components/UI/Header.jsx';
