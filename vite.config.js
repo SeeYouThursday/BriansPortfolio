59dafefcf141ec6d849c141dc5cf6de2ac2e2c30
Brian <galyenb@vcu.edu>
2024-01-24 22:41:09 -0500

    fixed logo pathing

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  base: '/',
-  build: { rollupOptions: { external: ['react', 'react-dom'] } },
+  // base: '/',
+  // build: { rollupOptions: { external: ['react', 'react-dom'] } },
 });
