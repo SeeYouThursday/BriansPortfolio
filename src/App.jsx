235cfd51b953c392277bf493017e61b229a6b824
Brian <galyenb@vcu.edu>
2024-01-24 22:37:38 -0500

    moved css to top of imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // import Header from './components/Header';
 // import Background from './components/ui/Background';
-import { Outlet } from 'react-router-dom';
 import './app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet } from 'react-router-dom';
 import Footer from './Footer.jsx';
 import Header from './Header.jsx';
 
