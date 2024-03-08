ae4c0adb98ff9ce5f1921916cf0c0d5b824f9159
Brian <galyenb@vcu.edu>
2024-03-08 10:12:56 -0500

    removed app.css import from App.jsx that caused netlifys build to fail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 // import Header from './components/Header';
 // import Background from './components/ui/Background';
 import { Outlet } from 'react-router-dom';
-import './assets/css/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/UI/Footer.jsx';
 import Header from './components/UI/Header.jsx';
