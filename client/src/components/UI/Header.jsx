97aa113fb3930536ee03df3dd544beeecc954604
Brian <galyenb@vcu.edu>
2024-03-09 12:15:23 -0500

    fixed Nav active tab bug with LocalStor/useEff/useState w/onClickfunc

--- client/src/components/UI/Header.jsx
+++ client/src/components/UI/Header.jsx
@@ -1,57 +1,83 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../assets/images/seeyouthursday.png';
 import { Link } from 'react-router-dom';
-
+import { useState, useEffect } from 'react';
 function Header() {
-  const style = { color: '#ffffff' };
+  //?? Handles Setting Active Tab even when refreshing/revisiting
+  const [activeTab, setActiveTab] = useState(
+    localStorage.getItem('activeTab') || '/'
+  );
 
+  useEffect(() => {
+    localStorage.setItem('activeTab', activeTab);
+  }, [activeTab]);
+
+  const style = { color: '#ffffff' };
   return (
     <Navbar
       collapseOnSelect
       expand="lg"
-      className="bg-body-tertiary mb-4"
+      className="bg-body-tertiary mb-4 d-flex flex-row justify-content-between"
       // fixed="top"
       bg="dark"
       data-bs-theme="dark"
       variant="tabs"
     >
-      <Container>
-        <Navbar.Brand href="/" id="navbar">
+      {/*//?? Logo and Name //?? */}
+      <div className="d-flex flex-row">
+        <Navbar.Brand href="/" id="navbar" style={{ marginLeft: 10 }}>
           <img
             alt="SeeYouThursday GitHub Logo"
             src={logo}
             width="30"
             height="30"
-            className="d-inline-block align-top"
+            className="d-inline-block align-top mb-2"
           />{' '}
         </Navbar.Brand>
         <h1 style={style}>Brian Galyen</h1>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" style={style} />
-        <Navbar.Collapse id="responsive-navbar-nav" style={style}>
-          <Nav variant="underline" defaultActiveKey="/" className="m-auto">
-            <Nav.Link style={style} as={Link} to="/" eventKey="/">
-              About Me
-            </Nav.Link>
-            {/* May map over an array*/}
-            <Nav.Link
-              style={style}
-              as={Link}
-              to="/Portfolio"
-              eventKey="/Portfolio"
-            >
-              Portfolio
-            </Nav.Link>
-            <Nav.Link style={style} as={Link} to="/Contact" eventKey="/Contact">
-              Contact Me
-            </Nav.Link>
-            <Nav.Link style={style} as={Link} to="/Resume" eventKey="link-4">
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      </div>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" style={style} />
+      <Navbar.Collapse id="responsive-navbar-nav" style={style}>
+        {/* //?? Nav Links //?? */}
+        <Nav variant="underline" activeKey={activeTab}>
+          <Nav.Link
+            style={style}
+            as={Link}
+            to="/"
+            eventKey="/"
+            onClick={() => setActiveTab('/')}
+          >
+            About Me
+          </Nav.Link>
+          <Nav.Link
+            style={style}
+            as={Link}
+            to="/Portfolio"
+            eventKey="/Portfolio"
+            onClick={() => setActiveTab('/Portfolio')}
+          >
+            Portfolio
+          </Nav.Link>
+          <Nav.Link
+            style={style}
+            as={Link}
+            to="/Contact"
+            eventKey="/Contact"
+            onClick={() => setActiveTab('/Contact')}
+          >
+            Contact Me
+          </Nav.Link>
+          <Nav.Link
+            style={style}
+            as={Link}
+            to="/Resume"
+            eventKey="Resume"
+            onClick={() => setActiveTab('/Resume')}
+          >
+            Resume
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
